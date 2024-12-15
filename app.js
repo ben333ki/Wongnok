4690814ab335c5b0ca88f039e6063bef25a40edc
popHello32154 <nawin.bu33@gmail.com>
2024-12-15 20:06:37 +0700

    a

---
+++ app.js
@@ -0,0 +1,38 @@
+// const express = require('express');
+// const app = express();
+
+// // Set EJS as the template engine
+// app.set('view engine', 'ejs');
+
+// // Route to render an EJS view
+// app.get('/', (req, res) => {
+//   res.render('index', { message: 'Hello, EJS!' });
+// });
+
+// // Start the server
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log('Server is running on http://localhost:${PORT}');
+// });
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('user/post', { title: 'Home Page' });
+});
+
+app.use(express.static('public'));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(path.join(__dirname, 'views', 'partials')); 
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
