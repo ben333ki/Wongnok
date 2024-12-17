5eb9c2fe1c388a71396aca5a0e0f2c958340108a
popHello32154 <nawin.bu33@gmail.com>
2024-12-17 21:24:07 +0700

    fe

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('user/home', { title: 'Home Page' });
+    res.render('user/create', { title: 'Home Page' });
 });
 
 app.use(express.static('public'));
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(path.join(__dirname, 'views', 'partials')); 
+    console.log(path.join(__dirname, 'views', 'partials'));
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
